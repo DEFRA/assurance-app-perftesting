d49d74ab43f5f6358c701dd22de201080d3c67c1
Loganathan Ekambaram <loganathan.ekambaram@defra.gov.uk>
2026-05-20 15:56:12 +0100

    Update entrypoint.sh and JMX file

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ JM_LOGS=${JM_HOME}/logs
 mkdir -p ${JM_REPORTS} ${JM_LOGS}
 
 TEST_SCENARIO=${TEST_SCENARIO:-test}
-SCENARIOFILE=${JM_SCENARIOS}/Assurance_App_SmokeTest_V0.1.jmx
+SCENARIOFILE=${JM_SCENARIOS}/Assurance_App_PeakTest_V0.1.jmx
 REPORTFILE=${NOW}-perftest-${TEST_SCENARIO}-report.csv
 LOGFILE=${JM_LOGS}/perftest-${TEST_SCENARIO}.log
 
